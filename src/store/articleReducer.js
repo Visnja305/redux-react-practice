57905a785d1b34d8c72378e2e5d7dc9f36b9d7ea
Visnja305 <105449754+Visnja305@users.noreply.github.com>
2023-10-28 13:33:05 -0400

    Final commit

--- src/store/articleReducer.js
+++ src/store/articleReducer.js
@@ -1,6 +1,7 @@
-import articles from '../data/data.json';
+import articles from "../data/data.json";
 
-const LOAD_ARTICLES = 'article/loadArticles';
+const LOAD_ARTICLES = "article/loadArticles";
+const ADD_ARTICLE = "article/addArticle";
 
 export const loadArticles = () => {
   return {
@@ -8,7 +9,10 @@ export const loadArticles = () => {
     articles
   };
 };
-
+export const addArticle = (payload) => ({
+  type: ADD_ARTICLE,
+  payload
+});
 
 const initialState = { entries: [], isLoading: true };
 
@@ -16,6 +20,8 @@ const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
       return { ...state, entries: [...action.articles] };
+    case ADD_ARTICLE:
+      return { ...state, entries: [...state.entries, action.payload] };
     default:
       return state;
   }
